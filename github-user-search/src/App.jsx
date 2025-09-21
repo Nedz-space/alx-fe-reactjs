5aaa8655bd01c3450031e7547a5d8aba23a21493
Nedz-space <0xnedz@gmail.com>
2025-09-21 08:41:47 +0100

    created github-user-search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>GitHub User Search</h1>
+      <p>App setup complete. Ready to build!</p>
+    </div>
+  );
+}
+
+export default App;
